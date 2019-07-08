a7b10f724338241c139c5cae60a8f5236fee3898
zhangmingming <zhangmingmig@adpanshi.com>
2019-07-08 15:01:48 +0800

    AccountConfig配置

--- src/main/java/com/zmm/sell/config/WeChatAccountConfig.java
+++ src/main/java/com/zmm/sell/config/WeChatAccountConfig.java
@@ -17,32 +17,32 @@ import java.util.Map;
 public class WeChatAccountConfig {
 
 	/**
-	 * 公众平台id
+	 * 公众平台id  wxd0bf9016c64152bf
 	 */
 	private String mpAppId;
 
 	/**
-	 * 公众平台密钥
+	 * 公众平台密钥   647b06336a6190d7fca07d9acd128c72
 	 */
 	private String mpAppSecret;
 
 	/**
-	 * 开放平台id
+	 * 开放平台id   wx5b3c9ce081ac568f
 	 */
 	private String openAppId;
 
 	/**
-	 * 开放平台密钥
+	 * 开放平台密钥   2d617525e280e02c2ec9ddaf5040b64a
 	 */
 	private String openAppSecret;
 
 	/**
-	 * 商户号
+	 * 商户号   1517307141
 	 */
 	private String mchId;
 
 	/**
-	 * 商户密钥
+	 * 商户密钥   2b59704d7f8644cbb9c9979a5981648f
 	 */
 	private String mchKey;
 
